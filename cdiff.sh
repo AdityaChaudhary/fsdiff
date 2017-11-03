4fcf923d6cbeeca2906f2dc3ecc30b06d536dfc3
Aditya Chaudhary <adityachaudharyfb@gmail.com>
2017-11-03 17:48:39 +0530

    Argument check added.

--- cdiff.sh
+++ cdiff.sh
@@ -1,18 +1,36 @@
 #!/bin/sh
 
 #current time
-start_time=$( date +%s.%N )
-echo "Press any key to end and list files:"
+#start_time=$( date +%s.%N )
+
+
+if [ $# -lt 2 ]
+  then
+    cat <<- _EOF_
+    [#] Insufficent arguments supplied
+
+    Usage: cdiff.sh <path>
+
+    path: path where to look for changes
+
+    Sample: cdiff.sh /root
+_EOF_
+exit
+fi
+
+
+SECONDS=0
+echo "Press any key to end and list changed files:"
 read inp
 #echo $inp
-bonus_time=0
-bonus_time=$2
+#bonus_time=0
+#bonus_time=$2
 
-elapsed_time=$( date +%s.%N --date="$start_time seconds ago" )
+#elapsed_time=$( date +%s.%N --date="$start_time seconds ago" )
+elapsed_time=$SECONDS
+echo "Changes in last $elapsed_time seconds"
+#elapsed_time=$( echo "$elapsed_time+$bonus_time" | bc -l)
 
-echo "Changes in" $elapsed_time "seconds + " $2 "second bonus"
-elapsed_time=$( echo "$elapsed_time+$bonus_time" | bc -l)
-
-echo $elapsed_time
-#find $1 -newermt "-$elapsed_time seconds" -ls
+#echo $elapsed_time
+find $1 -newermt "-$elapsed_time seconds" -ls
 #while
--- main.sh
+++ main.sh
@@ -2,4 +2,20 @@
 
 # ./main.sh path time
 
+if [ $# -lt 2 ]
+  then
+    cat <<- _EOF_
+    [#] Insufficent arguments supplied
+
+    Usage: main.sh <path> <time>
+
+    path: path where to look for changes
+    time: how long back to look. eg. -2 seconds, -2 minutes, -7 days
+
+    Sample: main.sh /root "-2 days"
+_EOF_
+exit
+fi
+
+
 find $1 -newermt "$2" -ls
